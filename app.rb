05c1e952d1bd57bbbda51385b1a6f5d1a5e2bab5
Student <example@example.com>
2019-12-17 09:00:05 -0800

    refactor album.rb, specs, and app.rb

---
+++ app.rb
@@ -0,0 +1,41 @@
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/album')
+require 'pry'
+also_reload('lib/**/*.rb')
+
+get('/') do
+  @albums = Album.all
+  erb(:albums)
+end
+
+get('/albums/:id/edit') do
+  @album = Album.find(params[:id].to_i())
+  erb(:edit_album)
+end
+
+get('/albums/new') do
+  erb(:new_album)
+end
+
+post('/albums') do
+  name = params[:album_name]
+  album = Album.new(name, nil)
+  album.save()
+  @albums = Album.all() # Adding this line will fix the error.
+  erb(:albums)
+end
+
+patch('/albums/:id') do
+  @album = Album.find(params[:id].to_i())
+  @album.update(params[:name])
+  @albums = Album.all
+  erb(:albums)
+end
+
+delete('/albums/:id') do
+  @album = Album.find(params[:id].to_i())
+  @album.delete()
+  @albums = Album.all
+  erb(:albums)
+end
